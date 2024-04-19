73499aa3461e54e701a3cd6a90c496c82a5e8bfc
Bangjie Deng <dengbangjie@foxmail.com>
2024-04-19 14:07:15 +0800

    add new file

---
+++ gcc/fortran/do_concurrent.cc
@@ -0,0 +1,18 @@
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "gfortran.h"
+#include "dependency.h"
+#include "constructor.h"
+#include "arith.h"
+#include "options.h"
+
+/**
+ * Resolve do concurrent loop in fortran.
+*/
+class DOConcurrentResolver
+{
+
+
+};
